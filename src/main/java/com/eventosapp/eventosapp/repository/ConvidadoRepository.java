3ed6369b0f6600820da05775238f3e6348d73db4
Andre Matheus <am_arao@hotmail.com>
2022-04-18 14:24:59 -0300

    listando convidados que confirma presença em evento x

--- src/main/java/com/eventosapp/eventosapp/repository/ConvidadoRepository.java
+++ src/main/java/com/eventosapp/eventosapp/repository/ConvidadoRepository.java
@@ -1,10 +1,11 @@
 package com.eventosapp.eventosapp.repository;
 
 import com.eventosapp.eventosapp.models.Convidado;
+import com.eventosapp.eventosapp.models.Evento;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String> {
 
-
+Iterable<Convidado> findByEvento(Evento evento);
 
 }
